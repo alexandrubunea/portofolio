9b5103cf498c3e496cb0fa3787a0b288f8e994be
Bunea Alexandru <alexandrubunea03@gmail.com>
2024-11-23 11:13:12 +0200

    Update A much simpler animation for the logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,7 +3,7 @@ import './Logo.css'
 function Logo({name, title}: {name: string; title: string}) {
   return (
     <>
-    <div className="skew-x-[25deg] bg-[#6F6BF2] p-8 bounce">
+    <div className="skew-x-[25deg] bg-[#6F6BF2] p-8 hover:-translate-x-10 duration-1000 mb-1">
         <h1 className="name text-bold uppercase text-6xl -skew-x-[25deg]">{name}</h1>
     </div>
     <div className="-skew-x-[25deg] skew-y-4 bg-[#474747] p-5 text-center hover:translate-x-10 duration-1000">
