5448bd1f035596a96f1379f2e3fc432db0ae205c
alexandru bunea <alexandrubunea03@gmail.com>
2024-11-24 14:05:25 +0200

    Update Added CRT effect.

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,18 @@ import Logo from './components/logo/Logo'
 function App() {
   return (
     <>
-      <div className="container mx-auto px-10 py-10">
-        <div className="flex flex-col lg:flex-row lg:justify-between gap-4">
-          <div className="w-full sm:w-auto order-2 lg:order-1">
-            <Menu />
-          </div>
-          <div className="w-full sm:w-auto order-1 lg:order-2">
-            <div className="row-auto">
-              <Logo name="Alexandru Bunea" title="Software Developer"/>
-            </div>
-            <div className="row-auto">
-              
+      <div className="">
+        <div className="container mx-auto px-10 py-10">
+            <div className="flex flex-col lg:flex-row lg:justify-between gap-4">
+              <div className="w-full sm:w-auto order-2 lg:order-1">
+                <Menu />
+              </div>
+              <div className="w-full sm:w-auto order-1 lg:order-2">
+                <div className="row-auto">
+                  <Logo name="Alexandru Bunea" title="Software Developer"/>
+                </div>
+                <div className="row-auto">
+              </div>
             </div>
           </div>
         </div>
--- src/components/menu_button/MenuButton.tsx
+++ src/components/menu_button/MenuButton.tsx
@@ -4,7 +4,7 @@ function MenuButton({ text }: { text: string }) {
   return (
     <>
       <button className="menu-button w-[10em] lg:w-[12em] h-[3em] rounded uppercase font-bold text-center 
-        p-2 transition-all duration-500 lg:hover:translate-x-10 my-3 shadow-md">
+        p-2 transition-all duration-500 my-3">
         {text}
       </button>
     </>
