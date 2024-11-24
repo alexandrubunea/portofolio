0b03839695c6cd23975d21e7d4b4bf777138f795
alexandru bunea <alexandrubunea03@gmail.com>
2024-11-24 14:34:02 +0200

    Update Buttons perspective changed.

--- src/components/menu_button/MenuButton.tsx
+++ src/components/menu_button/MenuButton.tsx
@@ -4,7 +4,7 @@ function MenuButton({ text }: { text: string }) {
   return (
     <>
       <button className="menu-button w-[10em] lg:w-[12em] h-[3em] rounded uppercase font-bold text-center 
-        p-2 transition-all duration-500 my-3">
+        p-2 transition-all duration-500 my-3 hover:-translate-y-2 lg:hover:translate-x-10 lg:hover:translate-y-0">
         {text}
       </button>
     </>
